877ae3ea378aa64955a18fe5581390ed36955949
jsgthb <jeff.schaack@gmail.com>
2022-11-26 01:01:10 +0100

    Moved register function to authController

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -16,3 +16,22 @@ exports.userLogin = async (req, res) => {
         return res.status(401).json({message: "Wrong username or password"})
     }
 }
+
+exports.userRegister = async (req, res) => {
+    // Check if request body is valid
+    const { username, password } = req.body
+    if (!username || !password) return res.status(400).json({message: "Username and password are required"})
+    // Check if user already exists
+    const duplicate = await User.findOne({username: username}).exec()
+    if (duplicate) return res.status(409).json({message: "User already exists"})
+    // Create user
+    try {
+        // Hash password with argon2
+        const passwordHash = await argon2.hash(password)
+        // Store new user & send response
+        const newUser = await User.create({username: username, password: passwordHash})
+        res.status(201).json({message: "User successfully created"})
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+}
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,11 +1,10 @@
 // User routes
 const express = require("express")
 const router = express.Router()
-const registerController = require("../controllers/registerController")
 const authController = require("../controllers/authController")
 
 // Register route
-router.get("/register", registerController.handleNewUser)
+router.get("/register", authController.userRegister)
 
 // Login route
 router.get("/login", authController.userLogin)
